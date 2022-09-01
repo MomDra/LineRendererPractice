e6e999067879c4c091e4f1ab0da35e80f86988d0
MomDra <yem9087@naver.com>
2022-09-02 00:56:39 +0900

    Part2 sin wave

--- LineRenderer/Assets/Scripts/LineRendererSinWave.cs
+++ LineRenderer/Assets/Scripts/LineRendererSinWave.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class LineRendererSinWave : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    float start = 0f;
+    [SerializeField]
+    float end = 5f;
+    [SerializeField][Range(5, 50)]
+    int points = 5;
+    [SerializeField][Min(1)]
+    float amplitude = 1;
+    [SerializeField][Min(1)]
+    float frequency = 1;
+
+    LineRenderer lineRenderer;
+
+    private void Awake() {
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        Play();
+    }
+
+    void Play(){
+        lineRenderer.positionCount = points;
+
+        for(int i = 0; i < points; ++i){
+            float t = (float)i / (points - 1);
+            float x = Mathf.Lerp(start, end, t);
+            float y = amplitude * Mathf.Sin(2 * Mathf.PI * t * frequency);
+
+            lineRenderer.SetPosition(i, new Vector3(x, y, 0));
+        }
     }
 }
